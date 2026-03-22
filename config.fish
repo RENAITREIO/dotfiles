e7ccf7b1e6b68c91e85b4e869dcab577a29337ef
RENAITRE <2543563791@qq.com>
2026-03-22 19:37:49 +0800

    added theme

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 fish_vi_key_bindings
 
+oh-my-posh init fish --config ~/dotfiles/atomic.omp.json | source
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
