f4747c26a43f1ea8ee1f61788a4f6106bd25e065
RENAITRE <2543563791@qq.com>
2026-03-30 22:21:47 +0800

    added nvim and deleted omp

--- config.fish
+++ config.fish
@@ -2,8 +2,6 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 fish_vi_key_bindings
 
-oh-my-posh init fish --config ~/dotfiles/atomic.omp.json | source
-
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
